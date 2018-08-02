bc99876d28f6db2ccd1b6bc8e8566c1719edc667
zwen000 <37707613+zwen000@users.noreply.github.com>
2018-08-02 16:02:07 -0400

    Add files via upload

---
+++ TIPCAL copy/TIPCAL/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  TIPCAL
+//
+//  Created by zw on 2018/8/2.
+//  Copyright © 2018 zw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func tapON(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTIP(_ sender: AnyObject) {
+        let tipPercent = [0.15, 0.18, 0.2]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercent[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
+}
+
